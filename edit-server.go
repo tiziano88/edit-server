3616d57e8b478bbf0e6f81dc5c98d554582d987d
Tiziano Santoro <tzn@google.com>
2014-09-03 10:06:10 +0100

    Error handling.

--- edit-server.go
+++ edit-server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,8 +13,8 @@ import (
 )
 
 var (
-	port    = flag.Int("port", 8989, "port to listen on.")
-	command = flag.String("command", "edit-in-gvim.sh", "command to run to open the editor. It will receive the temporary file name as first argument.")
+	port    = flag.Int("port", 7878, "port to listen on.")
+	command = flag.String("command", "./edit-in-gvim.sh", "command to run to open the editor. It will receive the temporary file name as first argument.")
 )
 
 func main() {
@@ -28,17 +29,21 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("request")
+
 	b := make([]byte, 100000)
 
 	n, err := r.Body.Read(b)
-	if err != nil {
-		log.Print("error reading request body: %v", err)
+	if err == io.EOF {
+		log.Printf("EOF")
+	} else if err != nil {
+		log.Printf("error reading request body: %v", err)
 		return
 	}
 
 	f, err := ioutil.TempFile("", "edit-server-")
 	if err != nil {
-		log.Print("error creating temporary file: %v", err)
+		log.Printf("error creating temporary file: %v", err)
 		return
 	}
 	defer f.Close()
@@ -47,23 +52,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	err = exec.Command(*command, f.Name()).Run()
 	if err != nil {
-		log.Print("error executing edit command %q: %v", *command, err)
+		log.Printf("error executing edit command %q: %v", *command, err)
 		return
 	}
 
 	if err := f.Sync(); err != nil {
-		log.Print("error reading temporary file %q: %v", f.Name(), err)
+		log.Printf("error reading temporary file %q: %v", f.Name(), err)
 		return
 	}
 
 	if _, err := f.Seek(0, 0); err != nil {
-		log.Print("error reading temporary file %q: %v", f.Name(), err)
+		log.Printf("error reading temporary file %q: %v", f.Name(), err)
 		return
 	}
 
 	n, err = f.Read(b)
 	if err != nil {
-		log.Print("error reading temporary file %q: %v", f.Name(), err)
+		log.Printf("error reading temporary file %q: %v", f.Name(), err)
 		return
 	}
 
